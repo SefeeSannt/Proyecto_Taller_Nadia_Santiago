8340fdaf6f31f0b52f8931ffc90b72f7e5ce82c7
Santiago_Perez <santyprez.sp@gmail.com>
2025-08-27 09:24:59 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_Taller_Nadia_Santiago
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
